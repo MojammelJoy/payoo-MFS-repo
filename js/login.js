a5b9304d1bc77b144abfb0b085efa523ec08753f
MojammelJoy <mojammeljoy2687@gmail.com>
2025-06-28 22:10:56 +0600

    added partially the add money event handler

--- js/login.js
+++ js/login.js
@@ -1,7 +1,26 @@
-document.getElementById('button-login').addEventListener('click', function(event){
-    event.preventDefault();
-    console.log('login button clicked')
+// document.getElementById('button-login').addEventListener('click', function(event){
+//     event.preventDefault();
 
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+// })
+
+document.getElementById('button-login')
+.addEventListener('click', function(event){
+ console.log('button clicked')
+ event.preventDefault();
+
+ const phoneNumber = document.getElementById('phone-number').value;
+ const pinNumber = document.getElementById('pin-number').value;
+
+//  console.log(phoneNumber, pinNumber);
+
+if(phoneNumber === '5' && pinNumber === '1234'){
+    console.log('your are logged in')
+    window.location.href = 'home.html';
+}
+else{
+    alert('Wrong phone number or pin')
+}
 })
